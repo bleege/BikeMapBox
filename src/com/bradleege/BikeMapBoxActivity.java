34518b576b06f57416036cda9e4a71676aec38b8
Brad Leege <bleege@gmail.com>
2013-10-15 17:41:58 -0500

    Initial project setup.

---
+++ src/com/bradleege/BikeMapBoxActivity.java
@@ -0,0 +1,17 @@
+package com.bradleege;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BikeMapBoxActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
