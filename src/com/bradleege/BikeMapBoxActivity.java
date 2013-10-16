b8b1bfd3b05a3aeccd7c1ea46a623b6f987ab3a6
Brad Leege <bleege@gmail.com>
2013-10-16 14:07:30 -0500

    Adding load and display route from local geojson data file.

--- src/com/bradleege/BikeMapBoxActivity.java
+++ src/com/bradleege/BikeMapBoxActivity.java
@@ -1,14 +1,29 @@
 package com.bradleege;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import com.bradleege.tilesource.MapBoxTileSource;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.PathOverlay;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BikeMapBoxActivity extends Activity
 {
+    private MapView mapView = null;
+
     /**
      * Called when the activity is first created.
      */
@@ -22,14 +37,69 @@ public class BikeMapBoxActivity extends Activity
         MapBoxTileSource mapBoxTileSource = new MapBoxTileSource("mbtiles", ResourceProxy.string.base, 1, 20, 256, ".png", "http://api.tiles.mapbox.com/v3/");
 
         // Interact With the MapView
-        MapView mapView = (MapView)findViewById(R.id.mapview);
+        mapView = (MapView)findViewById(R.id.mapview);
         mapView.setTileSource(mapBoxTileSource);
-/*
-        mapView.setBuiltInZoomControls(true);
-        mapView.setClickable(true);
-*/
         mapView.setMultiTouchControls(true);
-        mapView.getController().setZoom(15);
+        mapView.getController().setZoom(14);
         mapView.getController().setCenter(new GeoPoint(43.05277119900874, -89.42244529724121));
+
+        // Load The Route
+        new LoadRouteOntoMapTask().execute();
+    }
+
+    private class LoadRouteOntoMapTask extends AsyncTask<Void, Void, ArrayList<GeoPoint>>
+    {
+        @Override
+        protected ArrayList<GeoPoint> doInBackground(Void... params)
+        {
+            ArrayList<GeoPoint> points = new ArrayList<GeoPoint>();
+
+            try
+            {
+                // Load Data From geojson file
+                StringBuilder builder = new StringBuilder();
+
+                InputStream is = getResources().getAssets().open("map.geojson");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                String line;
+                while ((line = reader.readLine()) != null)
+                {
+                    builder.append(line);
+                }
+
+                JSONObject json = new JSONObject(builder.toString());
+                JSONArray coordinates = json.getJSONArray("features").getJSONObject(0).getJSONObject("geometry").getJSONArray("coordinates");
+
+                for (int lc = 0; lc < coordinates.length(); lc++)
+                {
+                    JSONArray c = coordinates.getJSONArray(lc);
+                    GeoPoint gp = new GeoPoint(c.getDouble(1), c.getDouble(0));
+                    points.add(gp);
+                }
+            }
+            catch (IOException e)
+            {
+                Log.e(LoadRouteOntoMapTask.class.getName(), "Error Reading JSON Data", e);
+            } catch (JSONException e)
+            {
+                Log.e(LoadRouteOntoMapTask.class.getName(), "Error Converting GeoJSON To GeoPoints", e);
+            }
+
+            return points;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<GeoPoint> geoPoints)
+        {
+            super.onPostExecute(geoPoints);
+
+            // Build and Display the Path on the map
+            PathOverlay myPath = new PathOverlay(Color.RED, getApplication().getApplicationContext());
+            for (GeoPoint gp : geoPoints)
+            {
+                myPath.addPoint(gp);
+            }
+            mapView.getOverlays().add(myPath);
+        }
     }
 }
